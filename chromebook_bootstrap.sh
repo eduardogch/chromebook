d11c2886d5efb83304bc8e261a51a15111a34d08
Eduardo Gonzalez <eduardo.gch@gmail.com>
2023-03-17 15:45:44 -0400

    Update chromebook_bootstrap.sh

--- chromebook_bootstrap.sh
+++ chromebook_bootstrap.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get -y update && sudo apt-get -y upgrade
 
-sudo apt-get -y install build-essential curl gnupg git git-core wget gpg
+sudo apt-get -y install build-essential curl git git-core wget
 
 
 #Node & NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-nvm install node
+nvm install 16
 nvm use node
 npm config set strict-ssl false
 npm config set registry http://registry.npmjs.org/
@@ -18,4 +18,4 @@ git config --global user.email eduardo.gch@gmail.com
 ssh-keygen -t rsa -C $USER"@localhost"
 
 # Clean system
-sudo apt-get -f install && apt-get autoremove && apt-get -y autoclean && apt-get -y clean
+sudo apt-get -f install && sudo apt-get autoremove && sudo apt-get -y autoclean && sudo apt-get -y clean
